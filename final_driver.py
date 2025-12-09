4eaf182f5906ed192e0499b7d2176aff2d9fda39
RFL Academy <competitions.mum@robofunlab.com>
2025-12-09 12:51:54 +0530

    final_driver.py

--- final_driver.py
+++ final_driver.py
@@ -143,6 +143,13 @@ motor_configs = {
         "back_right": {"pwm": 13, "dir": 14},
         "extra_motor": {"pwm": 4, "dir": 5},
     },
+    "THIRTEEN": {
+        "front_left": {"pwm": 25, "dir": 23},
+        "front_right": {"pwm": 18, "dir": 19},
+        "back_left": {"pwm": 17, "dir": 16},
+        "back_right": {"pwm": 13, "dir": 14},
+        "extra_motor": {"pwm": 4, "dir": 5},
+    },
     "SIXTEEN": {
         "front_left": {"pwm": 5, "dir": 4},
         "front_right": {"pwm": 19, "dir": 18},
